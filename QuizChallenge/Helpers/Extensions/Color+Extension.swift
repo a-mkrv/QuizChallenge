d2b745942adb20154db5245810d81b552dda33a8
Makarov Anton <ant.makarovv@gmail.com>
2019-01-22 18:31:45 +0300

    Update extension

---
+++ QuizChallenge/Helpers/Extensions/Color+Extension.swift
@@ -0,0 +1,32 @@
+//
+//  Color+Extension.swift
+//  QuizChallenge
+//
+//  Created by Anton Makarov on 22/01/2019.
+//  Copyright © 2019 Anton Makarov. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    convenience init(red: Int, green: Int, blue: Int, a: CGFloat = 1.0) {
+        self.init(
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue) / 255.0,
+            alpha: a)
+    }
+    
+    convenience init(hex: Int, a: CGFloat = 1.0) {
+        self.init(
+            red: (hex >> 16) & 0xFF,
+            green: (hex >> 8) & 0xFF,
+            blue: hex & 0xFF,
+            a: a
+        )
+    }
+    
+    static let mainBlue = UIColor(red:0.18, green:0.42, blue:0.62, alpha:1.00)
+    static let mainGray = UIColor(red:0.89, green:0.89, blue:0.91, alpha:1.00)
+}
---
+++ QuizChallenge/Helpers/IBCustomView.swift
@@ -0,0 +1,64 @@
+//
+//  IBCustomView.swift
+//  QuizChallenge
+//
+//  Created by Anton Makarov on 22/01/2019.
+//  Copyright © 2019 Anton Makarov. All rights reserved.
+//
+
+import UIKit
+
+//MARK: UIView
+@IBDesignable class IBView: UIView {
+
+    @IBInspectable var cornerRadius: CGFloat = 0 { didSet { layer.cornerRadius = cornerRadius }}
+    @IBInspectable var borderWidth: CGFloat = 0 { didSet { layer.borderWidth = borderWidth }}
+    @IBInspectable var borderColor: UIColor = .clear { didSet { layer.borderColor = borderColor.cgColor }}
+}
+
+//MARK: UIButton
+@IBDesignable class IBButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 { didSet { sharedInit() }}
+    @IBInspectable var borderWidth: CGFloat = 0 { didSet { sharedInit() }}
+    @IBInspectable var borderColor: UIColor = .clear { didSet { sharedInit() }}
+    
+    @IBInspectable var shadowOffset = CGSize(width: 0, height: 0) { didSet { sharedInit() }}
+    @IBInspectable var shadowOpacity: Float = 0 { didSet { sharedInit() }}
+    @IBInspectable var shadowColor: UIColor = .clear { didSet { sharedInit() }}
+    @IBInspectable var shadowRadius: CGFloat = 0 { didSet { sharedInit() }}
+
+    func sharedInit() {
+        self.layer.cornerRadius = CGFloat(cornerRadius)
+        self.layer.borderWidth = borderWidth
+        self.layer.borderColor = borderColor.cgColor
+        self.layer.shadowColor = shadowColor.cgColor
+        self.layer.shadowOpacity = shadowOpacity
+        self.layer.shadowOffset = shadowOffset
+        self.layer.shadowRadius = shadowRadius
+        self.clipsToBounds = true
+    }
+}
+
+//MARK: UIImageView
+@IBDesignable class IBImageView: UIImageView {
+    @IBInspectable var cornerRadius: CGFloat = 0 { didSet { layer.cornerRadius = cornerRadius }}
+}
+
+//MARK: UITextField
+@IBDesignable class IBTextField: UITextField {
+    
+    @IBInspectable var lineWidth: CGFloat = 1.0 { didSet{ drawLines() } }
+    @IBInspectable var lineColor: UIColor = .black { didSet{ drawLines() } }
+    
+    func drawLines() {
+        add(line: CGRect(x: 0.0, y: frame.size.height - lineWidth, width: frame.size.width, height: lineWidth))
+    }
+    
+    private func add(line: CGRect) {
+        let border = CALayer()
+        border.frame = line
+        border.backgroundColor = lineColor.cgColor
+        layer.addSublayer(border)
+    }
+}
