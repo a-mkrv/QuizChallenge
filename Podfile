11802f858c5bb976c2c7a14c27f0c653b926042b
Makarov Anton <ant.makarovv@gmail.com>
2019-01-22 15:11:40 +0300

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,13 @@
+project 'QuizChallenge.xcodeproj'
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'QuizChallenge' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod "Koloda"
+  pod "Alamofire"
+
+end
---
+++ QuizChallenge/ViewController/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  QuizChallenge
+//
+//  Created by Anton Makarov on 22/01/2019.
+//  Copyright © 2019 Anton Makarov. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
