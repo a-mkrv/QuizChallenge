9916b7840ebf5a32eb2b2f0c1ad412bccef79131
Makarov Anton <ant.makarovv@gmail.com>
2019-01-23 14:13:51 +0300

    Add advertising and light status bar

---
+++ QuizChallenge/View/AdvertisingCell.swift
@@ -0,0 +1,17 @@
+//
+//  AdvertisingCell.swift
+//  QuizChallenge
+//
+//  Created by Anton Makarov on 23/01/2019.
+//  Copyright © 2019 Anton Makarov. All rights reserved.
+//
+
+import UIKit
+
+class AdvertisingCell: UICollectionViewCell {
+    
+    @IBOutlet weak var adIcon: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var countCoinsLabel: UILabel!
+    
+}
---
+++ QuizChallenge/ViewController/AdvertisingViewController.swift
@@ -0,0 +1,48 @@
+//
+//  AdvertisingViewController.swift
+//  QuizChallenge
+//
+//  Created by Anton Makarov on 23/01/2019.
+//  Copyright © 2019 Anton Makarov. All rights reserved.
+//
+
+import UIKit
+
+class AdvertisingViewController: UIViewController {
+    
+    let advertising = ["Watch Ad", "Share VK", "Share FB", "Share Tweet", "Buy 100", "Buy 300", "Buy 500"]
+    let adDescription = ["+30 coins", "+30 coins", "+30 coins", "+30 coins", "60 RUB", "150 RUB", "300 RUB"]
+    
+    @IBOutlet weak var adCollectionView: UICollectionView!
+    @IBOutlet weak var coinsCountLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func pressBack(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+}
+
+extension AdvertisingViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return advertising.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AdvertisingCell", for: indexPath as IndexPath) as! AdvertisingCell
+        
+        cell.adIcon.image = UIImage(named: "AdCoins")
+        cell.countCoinsLabel.text = adDescription[indexPath.row]
+        cell.nameLabel.text = advertising[indexPath.row]
+        
+        return cell
+    }
+}
