624f19890115713e687de773a66d3f1044d159c8
Makarov Anton <ant.makarovv@gmail.com>
2019-01-25 12:25:54 +0300

    Update API with generics and splitting access levels

--- QuizChallenge/Services/Network/NetworkManager.swift
+++ QuizChallenge/Services/Network/NetworkManager.swift
@@ -19,39 +19,45 @@ enum Result<Value> {
 
 class NetworkManager {
     
+    // MARK: Init Singleton
+    
     static let shared = NetworkManager()
     private var session = Alamofire.SessionManager(configuration: .default)
     private let baseUrl = "https://quizbackend.com"
-
+    
     private init() {
         let configuration = URLSessionConfiguration.default
         configuration.httpAdditionalHeaders = SessionManager.defaultHTTPHeaders
         configuration.timeoutIntervalForRequest = 20
         session = Alamofire.SessionManager(configuration: configuration)
     }
-
-    func getUserProfile <T: Object> (type: T.Type, completion: @escaping (Result<Bool>) -> Void) -> Void where T:Mappable, T:Endpoint {
+    
+    // MARK: - Open Methods
+    func getUser(completion: @escaping (Result<Bool>) -> Void) {
+        getRequest(type: User.self, completion: completion)
+    }
+    
+    // ... get / create other object
+    
+    // MARK: - Private request logic
+    private func getRequest <T: Object> (type: T.Type, completion: @escaping (Result<Bool>) -> Void) -> Void where T:Mappable, T:Endpoint {
         
-        session.request(type.url()).responseArray { (response: DataResponse<[T]>) in
+        session.request(type.url()).responseObject { (response: DataResponse<T>) in
             
             switch response.result {
-            case .success(let items):
-                //TODO: Rewrite to separate storage class
-                    do {
-                        let realm = try Realm()
-                        try realm.write {
-                            for item in items {
-                                realm.add(item, update: true)
-                            }
-                        }
-                        completion(.success(true))
-                    } catch let error as NSError {
-                        completion(.error(error.description, error.code))
-                    }
+            case .success(let item):
+                do {
+                    try RealmManager.shared.storeObject(item)
+                    completion(.success(true))
+                } catch let error as NSError {
+                    completion(.error(error.description, error.code))
+                }
+                
             case .failure(let error as NSError):
                 completion(.error(error.description, error.code))
             }
         }
     }
     
+    // ... post
 }
