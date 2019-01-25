666cc3b032094516ff3cb28cd7114fce7f156533
Makarov Anton <ant.makarovv@gmail.com>
2019-01-25 10:23:48 +0300

    Add API request for Realm

--- QuizChallenge/Services/Network/NetworkManager.swift
+++ QuizChallenge/Services/Network/NetworkManager.swift
@@ -8,12 +8,20 @@
 
 import Foundation
 import Alamofire
+import ObjectMapper
+import AlamofireObjectMapper
+import RealmSwift
+
+enum Result<Value> {
+    case success(Value)
+    case error(String, Int)
+}
 
 class NetworkManager {
     
     static let shared = NetworkManager()
     private var session = Alamofire.SessionManager(configuration: .default)
-    private let baseUrl = "https://google.com"
+    private let baseUrl = "https://quizbackend.com"
 
     private init() {
         let configuration = URLSessionConfiguration.default
@@ -22,4 +30,28 @@ class NetworkManager {
         session = Alamofire.SessionManager(configuration: configuration)
     }
 
+    func getUserProfile <T: Object> (type: T.Type, completion: @escaping (Result<Bool>) -> Void) -> Void where T:Mappable, T:Endpoint {
+        
+        session.request(type.url()).responseArray { (response: DataResponse<[T]>) in
+            
+            switch response.result {
+            case .success(let items):
+                //TODO: Rewrite to separate storage class
+                    do {
+                        let realm = try Realm()
+                        try realm.write {
+                            for item in items {
+                                realm.add(item, update: true)
+                            }
+                        }
+                        completion(.success(true))
+                    } catch let error as NSError {
+                        completion(.error(error.description, error.code))
+                    }
+            case .failure(let error as NSError):
+                completion(.error(error.description, error.code))
+            }
+        }
+    }
+    
 }
