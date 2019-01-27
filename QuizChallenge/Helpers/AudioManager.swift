bdf46b1c8e74f810a3389a34816b8445f59e8def
Makarov Anton <ant.makarovv@gmail.com>
2019-01-27 15:47:19 +0300

    Add background sound player

---
+++ QuizChallenge/Helpers/AudioManager.swift
@@ -0,0 +1,48 @@
+//
+//  AudioManager.swift
+//  QuizChallenge
+//
+//  Created by Anton Makarov on 27/01/2019.
+//  Copyright © 2019 Anton Makarov. All rights reserved.
+//
+
+import AVFoundation
+
+class AudioManager {
+    
+    static let shared = AudioManager()
+    private init() { }
+    
+    private var audioPlayer: AVAudioPlayer?
+
+    func playMusic() {
+        if isPlaying()  {
+            print("Player Already Playing")
+            return
+        }
+        
+        let fileUrl = Bundle.main.url(forResource: "BackgroundMusic", withExtension: "mp3")
+        
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOf: fileUrl!)
+            audioPlayer?.numberOfLoops = -1
+            audioPlayer?.prepareToPlay()
+            audioPlayer?.play()
+            
+        } catch let error as NSError {
+            print("Error playing: ", error.description)
+        }
+    }
+    
+    func stopMuscic() {
+        isPlaying() ? audioPlayer!.stop() : ()
+    }
+    
+    private func isPlaying() -> Bool {
+        if let player = audioPlayer, player.isPlaying {
+            return true
+        }
+        
+        return false
+    }
+}
